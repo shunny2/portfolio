1e72e975841583c669c1bb2edc16bb7d50a66cbc
Alexander Davis <alexander.davis.098@gmail.com>
2022-10-05 19:40:08 -0300

    Fix: Fixing app structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import NavBar from './components/Navbar';
 import Banner from './components/Banner';
 import Skills from './components/Skills';
